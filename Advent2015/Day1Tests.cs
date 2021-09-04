92dcd7a2b863b18fb3fa4c309d6acb9936a98fa5
Clint Hoagland <bachelor.one@gmail.com>
2021-09-04 16:47:12 -0400

    sln and supporting files

---
+++ Advent2015/Day1Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Advent2015
+{
+    [TestFixture]
+    public class Day1Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
