5fa722322c9867c74ce8024318290f579441fce3
Clint Hoagland <bachelor.one@gmail.com>
2021-09-06 14:29:30 -0400

    Day 03 first part complete

--- Advent2015/Day03Tests.cs
+++ Advent2015/Day03Tests.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -12,23 +16,98 @@ namespace Advent2015
             var subject = new CountsPositions();
             subject.Count("").Should().Be(1);
         }
-        
+
         [Test]
-        [Ignore("not ready")]
         public void Count_OneMove_Returns2()
         {
             var subject = new CountsPositions();
             int result = subject.Count(">");
             result.Should().Be(2);
         }
+
+        [Test]
+        public void Count_FourMovesBackToHome_Returns4()
+        {
+            var subject = new CountsPositions();
+            int result = subject.Count("^>v<");
+            result.Should().Be(4);
+        }
+
+        [Test]
+        public void Count_MovesBetweenTwoHouses_Returns2()
+        {
+            var subject = new CountsPositions();
+            int result = subject.Count("^v^v^v^v^v");
+            result.Should().Be(2);
+        }
+
+        [Test]
+        public void Count_UsesPuzzleInput_GetsAnswer1()
+        {
+            var input = File.ReadAllText("C:\\Projects\\Homework\\advent-of-code-2015\\Advent2015\\input-day3.txt");
+            var subject = new CountsPositions();
+            int result = subject.Count(input);
+            result.Should().Be(2081);
+        }
+    }
+
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 
     public class CountsPositions
     {
-        private int result = 1;
+        private readonly List<Position> _positions;
+        private Position _currentPosition;
+        
+        public CountsPositions()
+        {
+            Position home = new Position(0, 0);
+            _positions = new List<Position> {home};
+            _currentPosition = home;
+        }
+
         public int Count(string input)
         {
-            return result;
+            if (string.IsNullOrWhiteSpace(input)) return _positions.Distinct().Count();
+            foreach (var move in input)
+            {
+                var nextPosition = new Position(_currentPosition.X, _currentPosition.Y);
+                    
+                switch (move)
+                {
+                    case '>':
+                        nextPosition.X++;
+                        break;
+                    case '<':
+                        nextPosition.X--;
+                        break;
+                    case '^':
+                        nextPosition.Y++;
+                        break;
+                    case 'v':
+                        nextPosition.Y--;
+                        break;
+                    default:
+                        throw new ArgumentException("not a valid move");
+                }
+
+                if (_positions.Exists(n => n.X == nextPosition.X && n.Y == nextPosition.Y) == false)
+                {
+                    _positions.Add(nextPosition);
+                }
+
+                _currentPosition = nextPosition;
+            }
+            return _positions.Count();
         }
     }
 }
