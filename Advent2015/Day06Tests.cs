09b67978ebf0e81bebaeb55c82f4221c8ecdab97
Clint Hoagland <bachelor.one@gmail.com>
2021-09-12 10:28:05 -0400

    parsing a command

--- Advent2015/Day06Tests.cs
+++ Advent2015/Day06Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.InteropServices;
 using FluentAssertions;
 using NUnit.Framework;
@@ -14,11 +15,66 @@ namespace Advent2015
             int result = subject.SumOfLitCells();
             result.Should().Be(0);
         }
+
+        [Test]
+        public void ParseCommand_TurnOn_GetsArgs()
+        {
+            var subject = new ParsesCommand();
+            var result = subject.Parse("turn on 0,0 through 999,999");
+            var expected = new GridCommand {Type = GridCommandType.TurnOn, X1 = 0, Y1 = 1, X2 = 999, Y2 = 999};
+            Assert.AreEqual(result.Type, GridCommandType.TurnOn);
+            result.X1.Should().Be(0);
+            result.Y1.Should().Be(0);
+            result.X2.Should().Be(999);
+            result.Y2.Should().Be(999);
+        }
+    }
+
+    public enum GridCommandType
+    {
+        TurnOn,
+        Toggle,
+        TurnOff
+    }
+    
+    public class GridCommand
+    {
+        public GridCommandType Type { get; set; }
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+    }
+
+    public class ParsesCommand
+    {
+        public GridCommand Parse(string input)
+        {
+            //turn on 0,0 through 999,999
+            var result = new GridCommand();
+            int typeLength = 0;
+            if (input.StartsWith("turn on"))
+            {
+                result.Type = GridCommandType.TurnOn;
+                typeLength = "turn on".Length;
+            }
+
+            var splitter = new string[] {"through"};
+            var tokens = input.Substring(typeLength).Split(splitter, StringSplitOptions.None);
+            var pos1 = tokens[0].Split(',');
+            result.X1 = int.Parse(pos1[0]);
+            result.Y1 = int.Parse(pos1[1]);
+            var pos2 = tokens[1].Split(',');
+            result.X2 = int.Parse(pos2[0]);
+            result.Y2 = int.Parse(pos2[1]);
+            
+            return result;
+        }
     }
 
     public class LightGrid
     {
-        private int[,] _cells = new int[999,999];
+        private int[,] _cells = new int[1000,1000];
         public int SumOfLitCells()
         {
             return 0;
